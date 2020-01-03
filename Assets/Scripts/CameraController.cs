cf307d3a87d38821aa9ce699a572e08859ac79a0
Jonay Gilabert López <jgl58@alu.ua.es>
2020-01-03 13:23:53 +0100

    Init

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 desplazamiento;
+    // Use this for initialization
+    void Start () {
+        desplazamiento = transform.position - player.transform.position;
+    }
+    // LateUpdate is called once per frame at the end
+    void LateUpdate () {
+        transform.position = player.transform.position + desplazamiento;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float velocidad;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void FixedUpdate ()
+    {
+        float posH = Input.GetAxis ("Horizontal");
+        float posV = Input.GetAxis ("Vertical");
+        Vector3 movimiento = new Vector3 (posH, 0.0f, posV);
+        rb.AddForce(movimiento * velocidad);
+    }
+}
